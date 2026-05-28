60b8e88d873d63e7d7181b7cca0072564c0cae2a
RajkishoreJena30 <rajkishore.jena@philips.com>
2026-05-28 17:07:59 +0530

    Code formated

--- ReactMachineCoding/redux-toolkit-practice/src/components/AddNewPostContainer.tsx
+++ ReactMachineCoding/redux-toolkit-practice/src/components/AddNewPostContainer.tsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { addAnewPost } from "../store/postsReducer";
 import type { AppDispatch } from "../store/store";
 
-const AddNewPostContainer = () =>{
+const AddNewPostContainer = () => {
     const dispatch = useDispatch<AppDispatch>();
 
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
 
-    const handleSubmit = (  e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        if(!title.trim() || !body.trim()){
+        if (!title.trim() || !body.trim()) {
             alert("Title and Body are required");
             return;
         }
@@ -21,14 +21,31 @@ const AddNewPostContainer = () =>{
         setBody("");
     }
 
-    return(
+    return (
         <div className="add-new-post-container">
             <h2>Add New Post</h2>
-           <form onSubmit={handleSubmit} className="new-post-form">
-              <input type="text" placeholder="Title" className="title" value={title} onChange={(e) => setTitle(e.target.value)} />
-              <textarea rows={7} placeholder="Body" className="body" value={body} onChange={(e)=>setBody(e.target.value)}></textarea>
-              <button className="submit-button" type="submit">Add Post</button>            
-           </form>
+            <form onSubmit={handleSubmit} className="new-post-form">
+                <input
+                    type="text"
+                    placeholder="Title"
+                    className="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <textarea
+                    rows={7}
+                    placeholder="Body"
+                    className="body"
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                />
+                <button
+                    className="submit-button"
+                    type="submit"
+                >
+                    Add Post
+                </button>
+            </form>
         </div>
     )
 }
